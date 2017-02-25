5c95ffdb696b0c435338627d4c0eba9aea9afed0
Alexey Golub <tyrrrrrr@gmail.com>
2017-02-25 16:33:27 +0200

    Add EnumToStringConverter

---
+++ WpfExtensions/Converters/EnumToStringConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+using System.Text.RegularExpressions;
+using System.Windows.Data;
+
+namespace Tyrrrz.WpfExtensions.Converters
+{
+    /// <summary>
+    /// Converts an enum value to <see cref="string"/>
+    /// </summary>
+    [ValueConversion(typeof(Enum), typeof(string))]
+    public class EnumToStringConverter : IValueConverter
+    {
+        /// <inheritdoc />
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var e = (Enum) value;
+            string str = e.ToString();
+            return Regex.Replace(str, @"([a-z])([A-Z])", "$1 $2");
+        }
+
+        /// <inheritdoc />
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string str = (string) value;
+            str = str.Replace(" ", "");
+            return Enum.Parse(targetType, str);
+        }
+    }
+}
